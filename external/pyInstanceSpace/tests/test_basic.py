b77fc84892794ed13571ba4cfc01fbda0a529821
Ethanwyf1 <yifwang13@student.unimelb.edu.au>
2025-04-17 23:14:50 +1000

    Replace broken submodule with direct code copy

---
+++ external/pyInstanceSpace/tests/test_basic.py
@@ -0,0 +1,22 @@
+"""Test cases are here."""
+
+import sys
+from pathlib import Path
+
+path_root = Path(__file__).parents[0]
+sys.path.append(str(path_root))
+
+
+def test_assertions() -> None:
+    """
+    The test case for demonstration.
+
+    Returns
+    -------
+        None
+
+    """
+    assert True, (
+        "Something is wrong with the Github Workflow - "
+        "please contact kharek@student.unimelb.edu.au"
+    )
